a281aef2df0bcceee1d8635590fc7878c01c2a13
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-17 10:50:24 +0200

    01.02. Handle an Unhandled Exception

---
+++ Source/Recipe_01_02/Recipe_01_02/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+using System;
+
+namespace Recipe_01_02
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender,
+            DispatcherUnhandledExceptionEventArgs e)
+        {
+            string message = string.Format("An unhandled exception has occurred.{0}{0}{1}",
+                Environment.NewLine, e.Exception);
+            MessageBox.Show(message, Application.ResourceAssembly.FullName);
+
+            //Handling this event will result in the application
+            //remaining alive. This is useful if you are able to
+            //recover from the exception.
+            e.Handled = true;
+        }
+    }
+}
---
+++ Source/Recipe_01_02/Recipe_01_02/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace Recipe_01_02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThrowHandledException_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnThrowUnhandledException_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnThrowUnhandledExceptionFromThread_Click(object sender, RoutedEventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.RunWorkerAsync();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
