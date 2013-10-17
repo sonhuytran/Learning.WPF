686a59ac244ce6466ba4ecd26cc0116eb7262ba1
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-17 14:16:03 +0200

    01.06. Add a PropertyChangedValueCallback to Any Dependency Property

---
+++ Source/Recipe_01_06/Recipe_01_06/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace Recipe_01_06
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DependencyPropertyDescriptor descriptor = DependencyPropertyDescriptor.FromProperty(
+                TextBox.TextProperty, typeof(TextBox));
+            descriptor.AddValueChanged(this.tbxEditMe, this.tbxEditMe_TextChanged);
+        }
+
+        private void tbxEditMe_TextChanged(object sender, EventArgs e)
+        {
+            this.tblkTextValue.Text = (sender as TextBox).Text;
+        }
+    }
+}
