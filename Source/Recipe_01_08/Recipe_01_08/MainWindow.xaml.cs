b25133f4a4f770e4673d948f37f75f4e6a6f9d71
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-18 11:26:16 +0200

    01.08. Create and Use an Attached Dependency Property

---
+++ Source/Recipe_01_08/Recipe_01_08/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+
+namespace Recipe_01_08
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void uiElement_Click(object sender, RoutedEventArgs e)
+        {
+            UIElement element = sender as UIElement;
+            MessageBox.Show("Rotation = " + GetRotation(element),
+                Application.ResourceAssembly.GetName().Name);
+        }
+
+        #region Rotation Attached Dependency Property
+
+        public static readonly DependencyProperty RotationProperty =
+            DependencyProperty.RegisterAttached("Rotation", typeof(double), typeof(MainWindow),
+                new FrameworkPropertyMetadata(0d,
+                    FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static void SetRotation(UIElement element, double value)
+        {
+            element.SetValue(MainWindow.RotationProperty, value);
+        }
+
+        public static double GetRotation(UIElement element)
+        {
+            return (double)element.GetValue(MainWindow.RotationProperty);
+        }
+
+        #endregion Rotation Attached Dependency Property
+
+        private void ListView_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Source/Recipe_01_08/Recipe_01_08/UserControl1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Recipe_01_08
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            UserControl1 control = sender as UserControl1;
+            MessageBox.Show("Rotation = " + MainWindow.GetRotation(control),
+                Application.ResourceAssembly.GetName().Name);
+        }
+    }
+}
