8dadcd0c73c847311d6278a27321189e3947b84e
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-21 18:34:56 +0200

    02.13. Popup

---
+++ Source/Recipe_02/Recipe_01_13/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Recipe_02_13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowPopup_Click(object sender, EventArgs e)
+        {
+            if (this.btnFadePopup == sender)
+            {
+                this.popRecipe02_13.PopupAnimation = PopupAnimation.Fade;
+            }
+            else if (this.btnScrollPopup == sender)
+            {
+                this.popRecipe02_13.PopupAnimation = PopupAnimation.Scroll;
+            }
+            else if (this.btnSlidePopup == sender)
+            {
+                this.popRecipe02_13.PopupAnimation = PopupAnimation.Slide;
+            }
+            else
+            {
+                this.popRecipe02_13.PopupAnimation = PopupAnimation.None;
+            }
+
+            this.popRecipe02_13.IsOpen = false;
+            this.popRecipe02_13.IsOpen = true;
+        }
+
+        private void btnClosePopup_Click(object sender, RoutedEventArgs e)
+        {
+            this.popRecipe02_13.IsOpen = false;
+        }
+    }
+}
