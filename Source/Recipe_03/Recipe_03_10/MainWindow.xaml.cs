1ebb2cc9ba108c07772cbf03e9b2c1cb1033da60
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-22 12:14:50 +0200

    03.10. RepeatButton & Slider

---
+++ Source/Recipe_03/Recipe_03_10/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Recipe_03_10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            this.sldr.Value--;
+        }
+
+        private void btnRight_Click(object sender, RoutedEventArgs e)
+        {
+            this.sldr.Value++;
+        }
+    }
+}
