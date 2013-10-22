f8d15aa07a5806c2f31c1ae1c1bf78adae0e8e07
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-22 11:05:52 +0200

    02.16. ToolBarTray, ToolBar, ApplicationCommands, EditingCommands

---
+++ Source/Recipe_02/Recipe_02_16/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Recipe_02_16
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
