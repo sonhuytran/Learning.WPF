e2220cc765b8e2f70160fd9e0f00f28230eb30b3
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-22 10:46:53 +0200

    02.15. Menu

---
+++ Source/Recipe_02/Recipe_02_15/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace Recipe_02_15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void miClear_Click(object sender, RoutedEventArgs e)
+        {
+            this.txtTextBox.Clear();
+        }
+
+        private void miTextStyles_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.miNormal == sender)
+            {
+                this.txtTextBox.FontWeight = FontWeights.Normal;
+                this.txtTextBox.FontStyle = FontStyles.Normal;
+            }
+            else if (this.miBold == sender)
+            {
+                this.txtTextBox.FontWeight = FontWeights.Bold;
+            }
+            else if (this.miItalic == sender)
+            {
+                this.txtTextBox.FontStyle = FontStyles.Italic;
+            }
+        }
+    }
+}
