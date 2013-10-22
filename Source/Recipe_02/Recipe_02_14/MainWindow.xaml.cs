425f7d4bfe6442bf5b96d86781fafdec1c8762a5
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-22 10:25:43 +0200

    02.14. Border

---
+++ Source/Recipe_02/Recipe_02_14/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Recipe_02_14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
