7febe2cfb9aaee1444f636e6b1ee247f0ca818a6
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-23 12:10:45 +0200

    04.01. UserControl, OpenFileDialog

---
+++ Source/Recipe_04/Recipe_04_01/FileInput.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace Recipe_04_01
+{
+    /// <summary>
+    /// Interaction logic for FileInput.xaml
+    /// </summary>
+    public partial class FileInput : UserControl
+    {
+        public FileInput()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+
+            if (openFileDlg.ShowDialog() == true)
+            {
+                this.SelectedFileName = openFileDlg.FileName;
+            }
+        }
+
+        #region SelectedFileName Dependency Property
+
+        public static DependencyProperty SelectedFileNameProperty =
+            DependencyProperty.Register("SelectedFileName", typeof(string), typeof(FileInput),
+                new FrameworkPropertyMetadata(null));
+
+        public string SelectedFileName
+        {
+            get
+            {
+                return this.GetValue(SelectedFileNameProperty) as string;
+            }
+
+            private set
+            {
+                this.SetValue(SelectedFileNameProperty, value);
+            }
+        }
+        
+        #endregion SelectedFileName Dependency Property
+    }
+}
