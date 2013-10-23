fb3371e23afe2c22708d8c8407ef7a9e52e5f1f7
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-23 11:26:43 +0200

    03.22. ListBox, ListBoxItem, SelectionChanged, Selected...

---
+++ Source/Recipe_03/Recipe_03_22/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Recipe_03_22
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Additional initialization
+            this.Title = Application.ResourceAssembly.GetName().Name;
+        }
+
+        private void outerListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object selectedItem = this.outerListBox.SelectedItem;
+            string message = string.Empty;
+
+            if (null == selectedItem)
+            {
+                message = "No item currently selected.";
+            }
+            else
+            {
+                message = string.Format("{0} is selected.",
+                    selectedItem);
+            }
+
+            this.tbkStatus.Text = message;
+        }
+
+        private void innerListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            if (null == sender
+                || !(sender is ListBoxItem))
+            {
+                return;
+            }
+
+            ListBoxItem selectedItem = sender as ListBoxItem;
+            string message = string.Format("{0} is selected.",
+                selectedItem.Content);
+            MessageBox.Show(message, this.Title);
+        }
+    }
+}
