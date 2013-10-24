2b2b8c3927c476f2806b27ec333976c2c13315e4
Son-Huy TRAN <sonhuytran@gmail.com>
2013-10-24 18:02:50 +0200

    05.07. Bind to a Method

---
+++ Source/Recipe_05/Recipe_05_07/DistanceConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Recipe_05_07
+{
+    enum DistanceType
+    {
+        Miles,
+        Kilometres
+    }
+
+    class DistanceConverter
+    {
+        public string Convert(double amount, DistanceType sourceType)
+        {
+            if (DistanceType.Miles.Equals(sourceType))
+            {
+                return string.Format("{0:0.##} km", amount * 1.609344);
+            }
+            else if (DistanceType.Kilometres.Equals(sourceType))
+            {
+                return string.Format("{0:0.##} miles", amount * 0.621371192);
+            }
+
+            throw new ArgumentOutOfRangeException("sourceType", sourceType,
+                "Source must be in Miles or Kilometres");
+        }
+    }
+
+    class DoubleToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                return ((double)value).ToString();
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                string input = value as string;
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    return 0;
+                }
+
+                return double.Parse(input);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
